6f267b6f8bbd6d964da92c6c1f35590ec3372f82
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2019-03-09 00:05:14 +0700

    Add files via upload

---
+++ loading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Loading());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Loading();
+    }
+
+    IEnumerator Loading()
+    {
+        yield return new WaitForSeconds(10);
+        Application.LoadLevel("menuutama");
+    }
+}
